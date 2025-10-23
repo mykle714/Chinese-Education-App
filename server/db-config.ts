eb2b373d0b792ac8f15ef923136bf948a22de5a3
Michael Ren <michaelren1928@gmail.com>
2025-10-23 00:30:08 -0700

    SSL

--- server/db-config.ts
+++ server/db-config.ts
@@ -12,5 +12,7 @@ export const config: PoolConfig = {
   max: 20, // Maximum number of clients in the pool
   idleTimeoutMillis: 30000, // Close idle clients after 30 seconds
   connectionTimeoutMillis: 2000, // Return an error after 2 seconds if connection could not be established
-  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false
+  ssl: process.env.NODE_ENV === 'production' && process.env.DB_HOST !== 'postgres' 
+    ? { rejectUnauthorized: false } 
+    : false
 };
